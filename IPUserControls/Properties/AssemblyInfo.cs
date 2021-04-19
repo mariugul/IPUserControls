8cd31659dd5fb499614e8f6d4ef9cb145da059b7
C. K <32439744+mariugul@users.noreply.github.com>
2021-04-19 23:23:39 +0200

    Update release information for nuget

--- IPUserControls/Properties/AssemblyInfo.cs
+++ IPUserControls/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IPUserControls")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WPF IP Address Field, Port and Status")]
+[assembly: AssemblyDescription("WPF User Controls with IP address field, IP port field and IP connection status icon.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Marius C. K.")]
 [assembly: AssemblyProduct("IPUserControls")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
