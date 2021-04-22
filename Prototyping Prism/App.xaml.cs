997d9d2cfd3d3685651b3c10f6beb5736e016354
C. K <32439744+mariugul@users.noreply.github.com>
2021-04-22 10:22:28 +0200

    Added a Prism MVVM test project

---
+++ Prototyping Prism/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prototyping_Prism.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace Prototyping_Prism
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ Prototyping Prism/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Prism.Mvvm;
+
+namespace Prototyping_Prism.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        private string _title = "Prism Application";
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _ipAddress = "0.0.0.100";
+
+        public string IpAddress
+        {
+            get
+            {
+                Debug.WriteLine($"_ipAddress in VM: {_ipAddress}");
+                return _ipAddress;
+            } 
+            set
+            {
+                SetProperty(ref _ipAddress, value);
+                Debug.WriteLine($"Set IpAddress in VM to: {_ipAddress}");
+            }
+        }
+    }
+}
